5669da56b0891ad35769d3d9966b1c1632794994
KshipraKode <kshiprakode@gmail.com>
2016-04-27 16:50:19 -0400

    Updated UI, Markers, SharedPreferences, etc

--- app/src/main/java/com/example/locationalert/MapsActivity.java
+++ app/src/main/java/com/example/locationalert/MapsActivity.java
@@ -1,23 +1,21 @@
 package com.example.locationalert;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -35,14 +33,14 @@ import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, LocationListener {
 
+    public static final String MyPREFERENCES = "LocationBump" ;
     private GoogleMap mMap;
-    double cLongitude, cLatitude;
-
-    Location location, finalAddress;
+    private Location finalAddress;
     private EditText locationName;
-    private TextView FinalAddress;
-    LocationManager locationManager;
-    Marker m;
+    private Marker m,destination;
+    private String destinationLongi,destinationLati,destinationName;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,10 +53,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Setting up Shared Preferences to save the last location and last search option
+        sp = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        editor = sp.edit();
+        destinationLongi = sp.getString("DestinationLongi", null);
+        destinationName = sp.getString("DestinationName", null);
+        destinationLati = sp.getString("DestinationLati", null);
+        editor.apply();
+        //Checking if your shared preferences have the last saved destination
+        finalAddress = new Location("Final Address");
+
+        if(destinationLati != null && destinationLongi != null) {
+            Log.d("Set Address",destinationLati + destinationLongi);
+            finalAddress.setLongitude(Double.parseDouble(destinationLongi));
+            finalAddress.setLatitude(Double.parseDouble(destinationLati));
+        }
+
         //Initializing the View elements like the Buttons and the TextViews
-        Button addButton = (Button) findViewById(R.id.buttonAddLocation);
+        ImageButton addButton = (ImageButton) findViewById(R.id.buttonAddLocation);
         locationName = (EditText) findViewById(R.id.location);
-        FinalAddress = (TextView) findViewById(R.id.finalLocation);
 
         //Activating the Listener on the Add Destination button
         addButton.setOnClickListener(this);
@@ -71,50 +84,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         //Using location Manager to get current location
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        //Check if the user has location permissions enabled for accessing the location
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+        //Check if the user has location permissions enabled for accessing the location
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //Setting the time after which the Android System should check the location - We have set it to 10 ms
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, Criteria.ACCURACY_FINE, this);
             mMap.setMyLocationEnabled(true);
         }
+
+        if(destination!=null)
+            destination.remove();
+        destination = mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(destinationLati),Double.parseDouble(destinationLongi))).title(locationName.toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title(destinationName));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(Double.parseDouble(destinationLati),Double.parseDouble(destinationLongi))));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        locationName.setText(destinationName);
+
+
     }
 
     public void onClick(View v) {
 
+        //If some location entered in the textbox
         if (!locationName.getText().toString().equals("")) {
 
-            //Use the
+            //Use the geocoder class to get the longitude and latitude of the location
             Geocoder geocoder = new Geocoder(getBaseContext());
             List<Address> gotAddresses = null;
             try {
-                gotAddresses = geocoder.getFromLocationName(locationName.getText().toString() + " near me", 1);
-            } catch (IOException e) {
+                gotAddresses = geocoder.getFromLocationName(locationName.getText().toString(), 1);
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
+            try{
+                if (gotAddresses != null) {
+                    if(gotAddresses.size()<=0)
+                    {
+                        Toast.makeText(MapsActivity.this, "Please Enter a valid Destination", Toast.LENGTH_SHORT).show();
+                        if(destination!=null)
+                            destination.remove();
+                        editor.putString("DestinationName",null);
+                        editor.putString("DestinationsLongi",null);
+                        editor.putString("DestinationLati", null);
 
-            try {
-                Address received = gotAddresses.get(0);
+                    }
+                    else {
 
-                mMap.addMarker(new MarkerOptions().position(new LatLng(received.getLatitude(), received.getLongitude())).title(locationName.toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(received.getLatitude(), received.getLongitude())));
-                mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+                        Address received = gotAddresses.get(0);
+                        Log.d("Destination", gotAddresses.get(0).toString());
+                        if(destination!=null)
+                            destination.remove();
+                        destination = mMap.addMarker(new MarkerOptions().position(new LatLng(received.getLatitude(), received.getLongitude())).title(locationName.toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).title(locationName.toString()));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(received.getLatitude(), received.getLongitude())));
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
 
-                finalAddress = new Location("Final Address");
-                finalAddress.setLatitude(received.getLatitude());
-                finalAddress.setLongitude(received.getLongitude());
 
-                View text1 = findViewById(R.id.location);
-                View text2 = findViewById(R.id.finalLocation);
-                text1.setVisibility(View.INVISIBLE);
-                v.setVisibility(View.INVISIBLE);
-                text2.setVisibility(View.VISIBLE);
+                        finalAddress.setLatitude(received.getLatitude());
+                        finalAddress.setLongitude(received.getLongitude());
+                        editor.putString("DestinationName",locationName.getText().toString());
+                        editor.putString("DestinationsLongi",String.valueOf(finalAddress.getLongitude()));
+                        editor.putString("DestinationLati",String.valueOf(finalAddress.getLatitude()));
+                        editor.commit();
 
-                FinalAddress.setText(received.getAddressLine(0));
-                Toast.makeText(MapsActivity.this, "You are " + location.distanceTo(finalAddress) + "from the Destination", Toast.LENGTH_SHORT).show();
-            } catch (NullPointerException e) {
-                Toast.makeText(MapsActivity.this, "Please specify destination address", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MapsActivity.this, "Destination Updated", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.d("ERROR","Address not found");
             }
         }
     }
@@ -122,16 +163,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onLocationChanged(Location location) {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+        //Remove any previous marker set
+        if(m!=null)
+            m.remove();
+
+
         m = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(12));
 
@@ -140,7 +176,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(MapsActivity.this, "You are " + location.distanceTo(finalAddress) + "from the Destination", Toast.LENGTH_SHORT).show();
             }
         }
-
     }
 
     @Override
